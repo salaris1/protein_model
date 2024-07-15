e187473bdac3d4a31f04779e3fad725be08fae70
salaris1 <sam.salari@roche.com>
2024-07-15 21:57:30 +0000

    adding masked model and embedding extractors

--- accelerated_maked.py
+++ accelerated_maked.py
@@ -16,35 +16,39 @@ from transformers import (
     Trainer,
     BitsAndBytesConfig
 )
-from transformers import EsmForSequenceClassification
+from transformers import EsmForMaskedLM
 from datasets import Dataset
 from accelerate import Accelerator, DistributedType
 from peft import get_peft_config, PeftModel, PeftConfig, get_peft_model, LoraConfig, TaskType, prepare_model_for_kbit_training
 import pickle
+from transformers import DataCollatorForLanguageModeling
+torch.cuda.empty_cache()
 
 # Initialize accelerator
 accelerator = Accelerator()
 
 # Set the CUDA visible devices based on the accelerator
 if accelerator.distributed_type == DistributedType.MULTI_GPU:
-    os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3,4"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3"
 
 os.environ["WANDB_NOTEBOOK_NAME"] = 'CAS_new.py'
-run = wandb.init(project='CAS_classification_2')
+run = wandb.init(project='CAS_classification_MLM')
 
 datafolder = "/home/salaris/protein_model/data/"
-modelfolder = "/home/salaris/protein_model/models/"
-base_modelfolder = modelfolder + "base_model_accelerated/"
-pickle_file_path = datafolder + "cas_data_512_v1/"  # --> where to save the files 
+modelfolder = "/home/salaris/protein_model/seq_models/"
+# modelname = "facebook/esm2_t6_8M_UR50D"
+modelname = "facebook/esm2_t30_150M_UR50D"
+# modelname = "facebook/esm1b_t33_650M_UR50S"
 
+base_modelfolder = modelfolder + modelname.split("/")[-1] +"_base_model_accelerated/"
+print("base_modelfolder: ", base_modelfolder)
 # Read and preprocess data
 df = pd.read_csv(datafolder + "all_data_20240704_14.csvtrain_test.csv", sep="\t")
 df['class'] = df['class'].astype('category')
 df['class'] = df['class'].cat.codes
 
 # Tokenization
-modelname = "facebook/esm2_t6_8M_UR50D"
-modelname_str = modelname.split("/")[1]
+modelname_str = modelname.split("/")[-1]
 tokenizer = AutoTokenizer.from_pretrained(modelname)
 
 max_sequence_length = 1024
@@ -65,7 +69,7 @@ def print_trainable_parameters(model):
 def preprocess_data(examples, max_length=512):
     text = examples["seq"]
     encoding = tokenizer(text, padding=True, truncation=True, max_length=max_length, is_split_into_words=False, add_special_tokens=False, return_tensors="pt")
-    encoding["labels"] = examples["class"]
+    #encoding["labels"] = examples["class"]
     return encoding
 
 encoded_dataset = dataset.map(
@@ -78,7 +82,8 @@ encoded_dataset = dataset.map(
 encoded_dataset.set_format("torch")
 
 # Model Loading and Setup for QLoRA
-model = EsmForSequenceClassification.from_pretrained(modelname, num_labels=13)
+model = EsmForMaskedLM.from_pretrained(modelname)
+
 model.save_pretrained(base_modelfolder)
 
 # model = prepare_model_for_kbit_training(model)
@@ -86,7 +91,7 @@ model.save_pretrained(base_modelfolder)
 print_trainable_parameters(model)
 
 lora_config = LoraConfig(
-    task_type=TaskType.SEQ_CLS, 
+    task_type=TaskType.CAUSAL_LM, 
     r=8, 
     lora_alpha=32, 
     lora_dropout=0.1, 
@@ -108,7 +113,8 @@ model = get_peft_model(model, lora_config)
 print_trainable_parameters(model)
 
 # Compute class weights due to class imbalance if necessary
-labels = np.array(encoded_dataset['train']['labels'])
+# labels = np.array(encoded_dataset['train']['labels'])
+labels = np.array(df['class'].tolist())
 class_weights = compute_class_weight('balanced', classes=np.unique(labels), y=labels)
 class_weights = torch.tensor(class_weights, dtype=torch.float)
 
@@ -116,21 +122,21 @@ class_weights = torch.tensor(class_weights, dtype=torch.float)
 loss_fct = nn.CrossEntropyLoss(weight=class_weights.to(accelerator.device))
 
 # Modify compute_loss in Trainer to use the custom loss function
-class CustomTrainer(Trainer):
-    def compute_loss(self, model, inputs, return_outputs=False):
-        labels = inputs.get("labels")
-        outputs = model(**inputs)
-        logits = outputs.get('logits')
-        loss = loss_fct(logits.view(-1, self.model.config.num_labels), labels.view(-1))
-        return (loss, outputs) if return_outputs else loss
+# class CustomTrainer(Trainer):
+#     def compute_loss(self, model, inputs, return_outputs=False):
+#         labels = inputs.get("labels")
+#         outputs = model(**inputs)
+#         logits = outputs.get('logits')
+#         loss = loss_fct(logits.view(-1, self.model.config.num_labels), labels.view(-1))
+#         return (loss, outputs) if return_outputs else loss
 
 # Trainer Configuration
 training_args = TrainingArguments(
     output_dir=modelfolder,
     overwrite_output_dir=True,
-    num_train_epochs=5,
-    per_device_train_batch_size=16,
-    per_device_eval_batch_size=16,
+    num_train_epochs=15,
+    per_device_train_batch_size=6,
+    per_device_eval_batch_size=6,
     warmup_steps=500,
     weight_decay=0.01,
     logging_dir='./logs',
@@ -146,22 +152,43 @@ training_args = TrainingArguments(
 )
 
 # Define metrics for evaluation
+from torch.nn.functional import cross_entropy
+
 def compute_metrics(eval_pred):
     logits, labels = eval_pred
     predictions = np.argmax(logits, axis=-1)
-    acc = accuracy_score(labels, predictions)
-    precision, recall, f1, _ = precision_recall_fscore_support(labels, predictions, average='weighted')
+
+    # Masked token accuracy
+    mask = labels != -100
+    masked_predictions = predictions[mask]
+    masked_labels = labels[mask]
+    accuracy = (masked_predictions == masked_labels).mean()
+
+    # Perplexity
+    loss = cross_entropy(
+        torch.tensor(logits).view(-1, logits.shape[-1]),
+        torch.tensor(labels).view(-1),
+        ignore_index=-100
+    ).item()
+    perplexity = np.exp(loss)
+
     return {
-        'accuracy': acc,
-        'f1': f1,
-        'precision': precision,
-        'recall': recall
+        'accuracy': accuracy,
+        'perplexity': perplexity
     }
 
+data_collator = DataCollatorForLanguageModeling(
+    tokenizer=tokenizer,
+    mlm=True,
+    mlm_probability=0.15
+)
+
+
 # Initialize the Trainer
-trainer = CustomTrainer(
+trainer = Trainer(
     model=model,
     args=training_args,
+    data_collator=data_collator,
     train_dataset=encoded_dataset['train'],
     eval_dataset=encoded_dataset['test'],
     tokenizer=tokenizer,
@@ -188,7 +215,6 @@ trainer.train(resume_from_checkpoint=checkpoint_path)
 model_path = os.path.join(modelfolder, "esm_finetuned_accelerated", run.name)
 model.save_pretrained(model_path)
 tokenizer.save_pretrained(model_path)
-
 
 # Save the model as a PyTorch model
 torch_model_path = os.path.join(modelfolder, "esm_finetuned_accelerated", run.name,"model.pth")
